53cbeb102364eced50d8bd548502040b5742153e
Ben Rogers <benrogers@mac.com>
2016-08-18 18:24:35 -0400

    First failing acceptance test for add in C

--- src/c/rnc.h
+++ src/c/rnc.h
@@ -1,4 +1,8 @@
 #ifndef RNC_H
 #define RNC_H
 
+#include <stddef.h>
+
+void rnc_add(char *sum, size_t sumlen, const char *num_l, const char *num_r);
+
 #endif /* !RNC_H */
--- test/acceptance/c/runtest.c
+++ test/acceptance/c/runtest.c
@@ -1,7 +1,14 @@
 #include "rnc.h"
 
+#include <string.h>
+
 int main(int argc, char **argv)
 {
+    char actual[256];
+
+    rnc_add(actual, sizeof(actual), "I", "I");
+    if (0 != strcmp("II", actual)) return 1;
+
     return 0;
     (void)argc;
     (void)argv;

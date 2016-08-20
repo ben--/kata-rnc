40d02c3da968528b0848e7fc778e72e4d1e0518e
Ben Rogers <benrogers@mac.com>
2016-08-20 12:30:24 -0400

    Rust acceptance test working with C->Rust wrapper

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,6 +1,21 @@
+extern crate libc;
+
+use std::os::raw::c_char;
+use libc::size_t;
+use libc::strncpy;
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
+    }
+}
+
+#[no_mangle]
+pub extern fn rnc_add(dst: *mut c_char, dstlen: size_t, num_l: *const c_char, num_r: *const c_char) {
+    let x = "II";
+
+    unsafe {
+        strncpy(dst, x.as_ptr() as *const i8, dstlen);
     }
 }

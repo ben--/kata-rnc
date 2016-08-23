5e5907459ef489cc92f100fb8ae04dfd3e9f1366
Ben Rogers <benrogers@mac.com>
2016-08-23 19:48:10 -0400

    Another simple sum for the acceptance test

--- test/acceptance/runtest.c
+++ test/acceptance/runtest.c
@@ -1,15 +1,29 @@
 #include "rnc.h"
 
+#include <stdio.h>
 #include <string.h>
+
+static int _sum(const char *l, const char *r, const char *expected_sum)
+{
+    char actual_sum[256];
+
+    rnc_add(actual_sum, sizeof(actual_sum), l, r);
+    if (0 != strcmp(expected_sum, actual_sum)) {
+        fprintf(stderr, "Failed adding %s + %s: %s != %s\n", l, r, actual_sum, expected_sum);
+        return 1;
+    }
+
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
-    char actual[256];
+    int errs = 0;
 
-    rnc_add(actual, sizeof(actual), "I", "I");
-    if (0 != strcmp("II", actual)) return 1;
+    errs += _sum("I", "I", "II");
+    errs += _sum("I", "II", "III");
 
-    return 0;
+    return errs;
     (void)argc;
     (void)argv;
 }

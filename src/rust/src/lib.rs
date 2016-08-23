6438ead813746b9f719169c5ce81acdb79c126f3
Ben Rogers <benrogers@mac.com>
2016-08-23 19:53:11 -0400

    Red tests for I + II

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -11,6 +11,13 @@ fn add_i_i() {
     assert_eq!("II", actual_value);
 }
 
+#[test]
+fn add_i_ii() {
+    let actual_value = _add("I", "II");
+
+    assert_eq!("III", actual_value);
+}
+
 #[no_mangle]
 pub extern fn rnc_add(dst: *mut c_char, dstlen: size_t, num_l: *const c_char, num_r: *const c_char) {
     let x = _add("", "");

4346ed706373107ca635550c7291eeec90010963
Ben Rogers <benrogers@mac.com>
2016-08-23 19:12:15 -0400

    First real failing C test (changes function signature)

--- src/c/rnc.h
+++ src/c/rnc.h
@@ -3,6 +3,6 @@
 
 #include <stddef.h>
 
-void rnc_add(char *sum, size_t sumlen, const char *num_l, const char *num_r);
+int rnc_add(char *sum, size_t sumlen, const char *num_l, const char *num_r);
 
 #endif /* !RNC_H */

20c7bd65ed870c4db5089750833effb0def55036
Ben Rogers <benrogers@mac.com>
2016-08-23 22:04:42 -0400

    Refactor C, driving in a compression function

--- src/c/rnc.c
+++ src/c/rnc.c
@@ -7,11 +7,18 @@ int rnc_add(char *sum, size_t sumlen, const char *num_l, const char *num_r)
     strcpy(sum, num_l);
     strcat(sum, num_r);
 
-    if (0 == strcmp("IIIII", sum)) {
-        strcpy(sum, "V");
+    rnc_compress(sum, sumlen, sum);
+
+    return 0;
+}
+
+int rnc_compress(char *compressed, size_t compressed_len, const char *uncompressed)
+{
+    if (0 == strcmp("IIIII", uncompressed)) {
+        strcpy(compressed, "V");
     }
 
     return 0;
 
-    (void)sumlen;
+    (void)compressed_len;
 }
--- src/c/rnc.h
+++ src/c/rnc.h
@@ -4,5 +4,6 @@
 #include <stddef.h>
 
 int rnc_add(char *sum, size_t sumlen, const char *num_l, const char *num_r);
+int rnc_compress(char *compressed, size_t compressed_len, const char *uncompressed);
 
 #endif /* !RNC_H */

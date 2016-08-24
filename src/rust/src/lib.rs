15b64f00dfbda1f0e415a08d904697fb9019e726
Ben Rogers <benrogers@mac.com>
2016-08-23 22:17:47 -0400

    Bring imports in line with rust style guide

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,11 +1,11 @@
-mod rnc;
-
 extern crate libc;
 
 use libc::size_t;
 use libc::strncpy;
 use std::ffi::CStr;
 use std::os::raw::c_char;
+
+mod rnc;
 
 #[no_mangle]
 pub extern fn rnc_add(dst: *mut c_char, dstlen: size_t, num_l: *const c_char, num_r: *const c_char) {

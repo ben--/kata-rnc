dec0c75b35d8f93ada90168d87ef0dd016a1c589
Ben Rogers <benrogers@mac.com>
2016-08-23 20:29:16 -0400

    Refactor to clean up lib.rs

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -5,29 +5,29 @@ use std::os::raw::c_char;
 use libc::size_t;
 use libc::strncpy;
 
-#[test]
-fn add_i_i() {
-    let actual_value = _add("I", "I");
+mod rnc {
+    #[test]
+    fn add_i_i() {
+        assert_eq!("II", add("I", "I"));
+    }
 
-    assert_eq!("II", actual_value);
+    #[test]
+    fn add_i_ii() {
+        assert_eq!("III", add("I", "II"));
+    }
+
+    pub fn add(num_l: &str, num_r: &str) -> String {
+        let sum = String::from(num_l);
+        return sum + num_r;
+    }
 }
 
-#[test]
-fn add_i_ii() {
-    let actual_value = _add("I", "II");
-
-    assert_eq!("III", actual_value);
-}
+use rnc::add;
 
 #[no_mangle]
 pub extern fn rnc_add(dst: *mut c_char, dstlen: size_t, num_l: *const c_char, num_r: *const c_char) {
     unsafe {
-        let sum = _add(CStr::from_ptr(num_l).to_str().unwrap(), CStr::from_ptr(num_r).to_str().unwrap());
+        let sum = add(CStr::from_ptr(num_l).to_str().unwrap(), CStr::from_ptr(num_r).to_str().unwrap());
         strncpy(dst, sum.as_ptr() as *const i8, dstlen);
     }
-}
-
-fn _add(num_l: &str, num_r: &str) -> String {
-    let sum = String::from(num_l);
-    return sum + num_r;
 }

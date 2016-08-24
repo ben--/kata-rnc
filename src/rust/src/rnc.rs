3f317ee2736d0ab367795ac8579b4d44910eb367
Ben Rogers <benrogers@mac.com>
2016-08-23 22:16:14 -0400

    Fix indent

--- src/rust/src/rnc.rs
+++ src/rust/src/rnc.rs
@@ -1,31 +1,30 @@
+pub fn add(num_l: &str, num_r: &str) -> String {
+    let sum = String::from(num_l) + num_r;
 
-    pub fn add(num_l: &str, num_r: &str) -> String {
-        let sum = String::from(num_l) + num_r;
+    if sum == "IIIII" {
+        String::from("V")
+    } else {
+        sum
+    }
+}
 
-        if sum == "IIIII" {
-            String::from("V")
-        } else {
-            sum
-        }
+#[cfg(test)]
+mod tests {
+    use super::add;
+
+    #[test]
+    fn add_i_i() {
+        assert_eq!("II", add("I", "I"));
     }
 
-    #[cfg(test)]
-    mod tests {
-        use super::add;
-
-        #[test]
-        fn add_i_i() {
-            assert_eq!("II", add("I", "I"));
-        }
-
-        #[test]
-        fn add_i_ii() {
-            assert_eq!("III", add("I", "II"));
-        }
-
-        #[test]
-        fn add_ii_iii() {
-            assert_eq!("V", add("II", "III"));
-
-        }
+    #[test]
+    fn add_i_ii() {
+        assert_eq!("III", add("I", "II"));
     }
+
+    #[test]
+    fn add_ii_iii() {
+        assert_eq!("V", add("II", "III"));
+
+    }
+}

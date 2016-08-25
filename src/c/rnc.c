5a35c08d3adcadf2abcc6cafca00fed49d287a70
Ben Rogers <benrogers@mac.com>
2016-08-25 15:53:08 -0400

    Simplest shuffling case in C (ordering of V/I)

--- src/c/rnc.c
+++ src/c/rnc.c
@@ -1,11 +1,25 @@
 #include "rnc.h"
 
+#include <stdbool.h>
 #include <string.h>
+
+static bool _comes_before(char l, char r)
+{
+    if (!r) return true;
+    return l == 'V';
+}
 
 int rnc_add(char *sum, size_t sumlen, const char *num_l, const char *num_r)
 {
-    strcpy(sum, num_l);
-    strcat(sum, num_r);
+    char *out = sum;
+    while (*num_l || *num_r) {
+        if (_comes_before(*num_l, *num_r)) {
+            *out++ = *num_l++;
+        } else {
+            *out++ = *num_r++;
+        }
+    }
+    *out = '\0';
 
     rnc_compress(sum, sumlen, sum);
 

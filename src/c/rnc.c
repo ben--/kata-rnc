56866c52ea700e901fae019469a5599918a0938f
Ben Rogers <benrogers@mac.com>
2016-08-25 22:03:57 -0400

    First denormalization in C

--- src/c/rnc.c
+++ src/c/rnc.c
@@ -9,8 +9,14 @@ static bool _comes_before(char l, char r)
     return l == 'V';
 }
 
-int rnc_add(char *sum, size_t sumlen, const char *num_l, const char *num_r)
+int rnc_add(char *sum, size_t sumlen, const char *raw_l, const char *raw_r)
 {
+    char buf_l[12], buf_r[12];
+    char *num_l = buf_l;
+    char *num_r = buf_r;
+    rnc_denormalize(num_l, sizeof(num_l), raw_l);
+    rnc_denormalize(num_r, sizeof(num_r), raw_r);
+
     char *out = sum;
     while (*num_l || *num_r) {
         if (_comes_before(*num_l, *num_r)) {
@@ -24,6 +30,18 @@ int rnc_add(char *sum, size_t sumlen, const char *num_l, const char *num_r)
     rnc_compress(sum, sumlen, sum);
 
     return 0;
+}
+
+int rnc_denormalize(char *out, size_t outlen, const char *normal)
+{
+    if (0 == strcmp("IV", normal)) {
+        strcpy(out, "IIII");
+    } else {
+        strcpy(out, normal);
+    }
+
+    return 0;
+    (void)outlen;
 }
 
 int rnc_compress(char *compressed, size_t compressed_len, const char *uncompressed)
--- src/c/rnc.h
+++ src/c/rnc.h
@@ -5,5 +5,6 @@
 
 int rnc_add(char *sum, size_t sumlen, const char *num_l, const char *num_r);
 int rnc_compress(char *compressed, size_t compressed_len, const char *uncompressed);
+int rnc_denormalize(char *out, size_t outlen, const char *normal);
 
 #endif /* !RNC_H */

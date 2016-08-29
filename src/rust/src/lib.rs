402243a5c1ae9ee346e590e594e3a22eba75629d
Ben Rogers <benrogers@mac.com>
2016-08-29 08:27:42 -0400

    Rename the remaining rnc.rs to add.rs

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -6,6 +6,9 @@ use std::ffi::CStr;
 use std::ffi::CString;
 use std::os::raw::c_char;
 
+mod add;
+pub use add::add;
+
 mod cmp;
 pub use cmp::cmp;
 
@@ -15,12 +18,10 @@ pub use denormalize::denormalize;
 mod normalize;
 pub use normalize::normalize;
 
-mod rnc;
-
 #[no_mangle]
 pub extern fn rnc_add(dst: *mut c_char, dstlen: size_t, num_l: *const c_char, num_r: *const c_char) {
     unsafe {
-        let sum = rnc::add(CStr::from_ptr(num_l).to_str().unwrap(), CStr::from_ptr(num_r).to_str().unwrap());
+        let sum = add(CStr::from_ptr(num_l).to_str().unwrap(), CStr::from_ptr(num_r).to_str().unwrap());
         let csum = CString::new(sum).unwrap();
         strncpy(dst, csum.as_ptr() as *const i8, dstlen);
     }

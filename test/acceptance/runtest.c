8a51ed8e0e99fbaeb4e257293b111e8b24e9f9e9
Ben Rogers <benrogers@mac.com>
2016-08-29 20:24:55 -0400

    Clean up acceptance test in anticipation of subtraction

--- test/acceptance/runtest.c
+++ test/acceptance/runtest.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-static int _sum(const char *l, const char *r, const char *expected_sum)
+static int _add(const char *l, const char *r, const char *expected_sum)
 {
     char actual_sum[256] = { 'u', 'n', 'i', 't', 'i', 'a', 'l', 'i', 'z', 'e', 'd' };
 
@@ -21,7 +21,7 @@ static int _sum(const char *l, const char *r, const char *expected_sum)
     return 0;
 }
 
-static int _fail(const char *l, const char *r, size_t buflen)
+static int _add_fail(const char *l, const char *r, size_t buflen)
 {
     char buf[256] = { 'u', 'n', 'i', 't', 'i', 'a', 'l', 'i', 'z', 'e', 'd' };
 
@@ -39,36 +39,36 @@ int main(int argc, char **argv)
 {
     int errs = 0, tests = 0;
 
-    tests++; errs += _sum("I", "I", "II");
-    tests++; errs += _sum("I", "II", "III");
-    tests++; errs += _sum("II", "III", "V");
-    tests++; errs += _sum("IV", "I", "V");
-    tests++; errs += _sum("V", "I", "VI");
-    tests++; errs += _sum("IV", "V", "IX");
-    tests++; errs += _sum("V", "V", "X");
-    tests++; errs += _sum("VI", "IV", "X");
-    tests++; errs += _sum("I", "IX", "X");
-    tests++; errs += _sum("IX", "X", "XIX");
-    tests++; errs += _sum("V", "XIV", "XIX");
-    tests++; errs += _sum("V", "XV", "XX");
-    tests++; errs += _sum("L", "I", "LI");
-    tests++; errs += _sum("L", "XI", "LXI");
-    tests++; errs += _sum("XLIX", "I", "L");
-    tests++; errs += _sum("XX", "XX", "XL");
-    tests++; errs += _sum("XL", "X", "L");
-    tests++; errs += _sum("L", "L", "C");
-    tests++; errs += _sum("XCIX", "II", "CI");
-    tests++; errs += _sum("CC", "CC", "CD");
-    tests++; errs += _sum("CDXCIX", "I", "D");
-    tests++; errs += _sum("DCD", "C", "M");
-    tests++; errs += _sum("DCDXCIX", "V", "MIV");
-    tests++; errs += _sum("DCCCXCIX", "X", "CMIX");
-    tests++; errs += _sum("MCMXC", "L", "MMXL");
-    tests++; errs += _sum("I", "MMMCMXCVIII", "MMMCMXCIX");
-    tests++; errs += _sum("MMMDCCCLXXXVIII", "I", "MMMDCCCLXXXIX");
+    tests++; errs += _add("I", "I", "II");
+    tests++; errs += _add("I", "II", "III");
+    tests++; errs += _add("II", "III", "V");
+    tests++; errs += _add("IV", "I", "V");
+    tests++; errs += _add("V", "I", "VI");
+    tests++; errs += _add("IV", "V", "IX");
+    tests++; errs += _add("V", "V", "X");
+    tests++; errs += _add("VI", "IV", "X");
+    tests++; errs += _add("I", "IX", "X");
+    tests++; errs += _add("IX", "X", "XIX");
+    tests++; errs += _add("V", "XIV", "XIX");
+    tests++; errs += _add("V", "XV", "XX");
+    tests++; errs += _add("L", "I", "LI");
+    tests++; errs += _add("L", "XI", "LXI");
+    tests++; errs += _add("XLIX", "I", "L");
+    tests++; errs += _add("XX", "XX", "XL");
+    tests++; errs += _add("XL", "X", "L");
+    tests++; errs += _add("L", "L", "C");
+    tests++; errs += _add("XCIX", "II", "CI");
+    tests++; errs += _add("CC", "CC", "CD");
+    tests++; errs += _add("CDXCIX", "I", "D");
+    tests++; errs += _add("DCD", "C", "M");
+    tests++; errs += _add("DCDXCIX", "V", "MIV");
+    tests++; errs += _add("DCCCXCIX", "X", "CMIX");
+    tests++; errs += _add("MCMXC", "L", "MMXL");
+    tests++; errs += _add("I", "MMMCMXCVIII", "MMMCMXCIX");
+    tests++; errs += _add("MMMDCCCLXXXVIII", "I", "MMMDCCCLXXXIX");
 
-    tests++; errs += _fail("I", "I", 2);
-    //tests++; errs += _fail("IL", "I", 32);
+    tests++; errs += _add_fail("I", "I", 2);
+    //tests++; errs += _add_fail("IL", "I", 32);
 
     printf("test result: %d passed, %d failed\n", tests - errs, errs);
 

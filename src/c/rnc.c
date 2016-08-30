d1d3ed1ffa7b99e013121c251ede712da601a5cb
Ben Rogers <benrogers@mac.com>
2016-08-29 21:41:40 -0400

    Acceptance tests for C and first non-trivial subtraction

--- src/c/rnc.c
+++ src/c/rnc.c
@@ -50,11 +50,9 @@ int rnc_add(char *sum, size_t sumlen, const char *raw_l, const char *raw_r)
 
 int rnc_sub(char *diff, size_t diff_len, const char *num_l, const char *num_r)
 {
-    strcpy(diff, "I");
+    strcpy(diff, num_l);
+    replace(diff, diff_len, num_r, strlen(num_r), "", strlen(""));
     return 0;
-    (void)diff_len;;
-    (void)num_l;
-    (void)num_r;
 }
 
 int rnc_denormalize(char *out, size_t outlen, const char *normal)

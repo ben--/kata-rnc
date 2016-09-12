269fef4250d27a4c1c7a5d98f89cb802e808cea2
Ben Rogers <benrogers@mac.com>
2016-09-12 08:00:17 -0400

    (Naive) sizing of the RHS buffer in rnc_sub()

--- src/c/rnc.c
+++ src/c/rnc.c
@@ -56,7 +56,7 @@ int rnc_add(char *sum, size_t sumlen, const char *raw_l, const char *raw_r)
 
 int rnc_sub(char *diff, size_t diff_len, const char *num_l, const char *num_r)
 {
-    char buf_l[32];
+    char buf_l[sizeof("MMMDCCCCLXXXXVIIIII")];
     char buf_r[sizeof("MMMDCCCCLXXXXVIIII")];
     rnc_denormalize(buf_l, sizeof(buf_l), num_l);
     rnc_denormalize(buf_r, sizeof(buf_r), num_r);
